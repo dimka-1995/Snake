e4cf7a2c1c08904aace555b1e8b48f7429010965
Dmitry_Naumov <vpn1992@yandex.ru>
2017-06-29 14:57:18 +0300

    lesson 3

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,23 +10,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            /*
             int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
-
+            */
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Draw(x1, y1, '*');
-            Draw(x2, y2, '#');
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        
     }
 }
